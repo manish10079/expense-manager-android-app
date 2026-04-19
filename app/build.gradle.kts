86c38fd942d5759a405711d823257cb2408730bc
Manishh Kumar Nayak <try.manish0079@gmail.com>
2026-04-19 17:00:12 +0530

    chore: update versionCode to 2 and versionName to 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.mkn0079.expensetracker"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
