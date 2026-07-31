9fc9b55f78d115dfdb6ca85036881b18fe72a0b3
Manishh Kumar Nayak <try.manish0079@gmail.com>
2026-07-31 10:13:07 +0530

    ...

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.mknlabs.expensetracker"
         minSdk = 24
         targetSdk = 36
-        versionCode = 95
-        versionName = "2.27.0.95"
+        versionCode = 96
+        versionName = "2.27.0.96"
 
         resValue("string", "label_app_version", "v$versionName")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
